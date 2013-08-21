25a00b4ce9277ea1515db39f6d7efd3a9d1f0b85
Zouhair <zouhair.elharrouchi@gmail.com>
2013-08-21 02:39:08 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+
+<body>
+	<form action="recogedor.php" enctype="multipart/form-data" method="post">
+		Nombre: <input type="text" name="nombre"/>
+		<br/>
+		Archivo a subir: <input type="file" name="archivo"/>
+		<br/>
+		<input type="submit" value="Enviar"/>
+	</form>
+    <img src='mostrarArchivo.php?id=1' />
+	
+</body>
+</html>
